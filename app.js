e4add08f777f41f7f20adc5c745d5a83a544e345
nahidallhv <161585836+nahidallhv@users.noreply.github.com>
2024-03-26 14:50:55 +0400

    Task1

---
+++ app.js
@@ -0,0 +1,13 @@
+// document.writeln("Hello world");
+// document.writeln("Nahid");
+// document.write(12);
+
+//console log
+console.log("Hello world");
+console.log(15);
+console.clear();
+
+let a = 5;
+let b = 10;
+
+console.log(a + b);
